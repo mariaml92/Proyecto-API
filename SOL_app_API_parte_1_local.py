8bff1d9b7c2780600a57785464201c3e38a53e21
mariaml92 <maria.mora.lopez@outlook.com>
2023-02-18 12:43:56 +0100

    Actualizar

--- SOL_app_API_parte_1_local.py
+++ SOL_app_API_parte_1_local.py
@@ -26,8 +26,8 @@ def get_predict():
     crs = conn.cursor()
 
     # Get POST JSON data
-    data = request.get_json()
-    if data==None:
+    data = request.get_json(silent= True)
+    if data == None:
         data=request.args
     tv = data.get("TV",0)
     radio = data.get("radio",0)
@@ -41,7 +41,7 @@ def get_predict():
                     VALUES(?,?,?,?,?) ''', (str_time, tv, radio, newspaper, pred))
     conn.commit()
     conn.close()
-    return str(pred), 200
+    return 'con valores %.1f %.1f %.1f: %.3f' %(tv, radio, newspaper, str(pred)), 200
 
 
 @app.route('/review_predicts', methods=['GET'])
